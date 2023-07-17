1c4507ab573097c75043381d523b6bc4cf5907aa
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-07-17 16:01:06 -0700

    animation projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,31 +1,32 @@
-import { Card, Grid, Text, Link, Container, Button } from "@nextui-org/react";
+import { useState } from "react";
+import { Card, Text, Link, Container, Button, Badge } from "@nextui-org/react";
 
 const ProjectsSection = () => {
   const projects = [
     {
       id: 1,
-      keyword: "HACKATHON website",
+      keyword: "Hackathon Website",
       name: "Oasis Hacks 2023",
       img: "https://media.istockphoto.com/id/165958447/vector/postcard-palms-sea.jpg?s=612x612&w=0&k=20&c=6A8hIdZwed0_6SOS5f14K25kvXioPxVOoxsOt6G2W5I=",
       link: "https://oasiswebsite.com",
     },
     {
       id: 2,
-      keyword: "EDUCATION",
+      keyword: "Education",
       name: "Tech Optimum",
       img: "https://www.techoptimum.org/learn-in-seconds.jpg",
       link: "https://techoptimum.org",
     },
     {
       id: 3,
-      keyword: "HACKATHON PROJECT",
+      keyword: "Hackathon Project",
       name: "Resumate",
       img: "https://i.pinimg.com/736x/a3/00/33/a3003310dc763123d4b5c067fe358535.jpg",
       link: "https://resumate.tech/",
     },
     {
       id: 4,
-      keyword: "E-commerce",
+      keyword: "E-Commerce",
       name: "Rabbit Meat Store",
       img: "https://lirp.cdn-website.com/b9056a0f/dms3rep/multi/opt/hillsborobarn-640w.jpeg",
       link: "https://maxxriley.com",
@@ -57,52 +58,88 @@ const ProjectsSection = () => {
       img: "https://wallpapers.com/images/hd/educational-items-aesthetic-bg8i103jk2cdd1gc.jpg",
       link: "https://dashboard.techoptimum.org",
     },
+    {
+      id: 9,
+      keyword: "E-COMMERCE",
+      name: "Launchpad Labs",
+      img: "https://wallpapers.com/images/hd/night-city-aesthetic-f2114yshb1p9dmgq.jpg",
+      link: "https://launchpadlabs.pro",
+    },
   ];
-  const renderCard = (project) => (
-    <Card css={{ w: "100%", h: "400px" }}>
-      <Card.Header css={{ position: "absolute", zIndex: 1, top: 5, left: 5 }}>
-        <div>
-          <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-            {project.keyword}
-          </Text>
-          <Text h3 color="white">
-            {project.name}
-          </Text>
-        </div>
-      </Card.Header>
-      <Card.Body css={{ p: 0 }}>
-        <Card.Image
-          src={project.img}
-          width="100%"
-          height="100%"
-          objectFit="cover"
-          alt={project.name}
-        />
-      </Card.Body>
-      <Card.Footer
-        isBlurred
-        css={{
-          position: "absolute",
-          borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-          bottom: 0,
-          zIndex: 1,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
+  const ProjectCard = ({ project }) => {
+    const [hovered, setHovered] = useState(false);
+
+    return (
+      <div
+        style={{
+          flex: hovered ? 2 : 1,
+          transition: "all 0.3s ease-in-out",
+          margin: "1em",
         }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
-        {project.link ? (
-          <Link block isExternal icon color="primary" target="_blank" href={project.link}>
-            Visit Website
-          </Link>
-        ) : (
-          <Text color="gray">No link</Text>
-        )}
-        <Button auto color="primary">
-          About this project
-        </Button>
-      </Card.Footer>
-    </Card>
+        <Card css={{ w: "100%", h: "400px" }}>
+          <Card.Header
+            css={{ position: "absolute", zIndex: 1, top: 5, left: 5 }}
+          >
+            <div>
+              <Badge disableOutline variant="flat">
+                {project.keyword}
+              </Badge>
+              <Text h3 color="white">
+                {project.name}
+              </Text>
+            </div>
+          </Card.Header>
+          <Card.Body css={{ p: 0 }}>
+            <Card.Image
+              src={project.img}
+              width="100%"
+              height="100%"
+              objectFit="cover"
+              alt={project.name}
+            />
+          </Card.Body>
+          <Card.Footer
+            isBlurred
+            css={{
+              position: "absolute",
+              borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+              bottom: 0,
+              zIndex: 1,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {project.link ? (
+              <Link
+                block
+                isExternal
+                icon
+                color="primary"
+                target="_blank"
+                href={project.link}
+              >
+                Visit Website
+              </Link>
+            ) : (
+              <Text color="gray">No link</Text>
+            )}
+            <Button auto color="primary">
+              About this project
+            </Button>
+          </Card.Footer>{" "}
+        </Card>
+      </div>
+    );
+  };
+
+  const rows = [...Array(Math.ceil(projects.length / 3))];
+
+  const projectRows = rows.map((row, idx) =>
+    projects.slice(idx * 3, idx * 3 + 3)
   );
 
   return (
@@ -118,34 +155,18 @@ const ProjectsSection = () => {
       >
         <Text h2>my projects</Text>
       </Container>
-      <Grid.Container
-        gap={2}
-        justify="center"
-        style={{ width: "65%", margin: "0 auto" }}
-      >
-        <Grid xs>{renderCard(projects[0])}</Grid>
-        <Grid xs>{renderCard(projects[1])}</Grid>
-      </Grid.Container>
-
-      <Grid.Container
-        gap={2}
-        justify="center"
-        style={{ width: "65%", margin: "0 auto" }}
-      >
-        <Grid xs>{renderCard(projects[2])}</Grid>
-        <Grid xs>{renderCard(projects[3])}</Grid>
-        <Grid xs>{renderCard(projects[4])}</Grid>
-      </Grid.Container>
-
-      <Grid.Container
-        gap={2}
-        justify="center"
-        style={{ width: "65%", margin: "0 auto" }}
-      >
-        <Grid xs={6}>{renderCard(projects[5])}</Grid>
-        <Grid xs>{renderCard(projects[6])}</Grid>
-        <Grid xs>{renderCard(projects[7])}</Grid>
-      </Grid.Container>
+      <div style={{ width: "65%", margin: "0 auto" }}>
+        {projectRows.map((row, idx) => (
+          <div
+            key={idx}
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            {row.map((project, index) => (
+              <ProjectCard key={index} project={project} />
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
