b645183d239b3d9138f0f0419c3ad361763f7cc8
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-07-19 16:22:16 -0700

    analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import { createTheme, NextUIProvider, Switch, useTheme } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useTheme as useNextTheme } from 'next-themes'
-
+import { Analytics } from '@vercel/analytics/react'
 const lightTheme = createTheme({
   type: "light",
   theme: {},
@@ -26,6 +26,7 @@ export default function App({ Component, pageProps }) {
     >
       <NextUIProvider>
         <Component {...pageProps} />
+        <Analytics/>
       </NextUIProvider>
     </NextThemesProvider>
   );
