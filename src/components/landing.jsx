3e1a13f926c842a6ddda71cc81f57e2234a7ffa3
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-07-23 22:04:54 -0700

    added lottie animation

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,26 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import { Container, Button } from "@nextui-org/react";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { motion } from "framer-motion";
+import Lottie from 'lottie-react';
 
 const LandingSection = () => {
   // Define the initial and animate states
   const initial = { opacity: 0, y: -100 };
   const animate = { opacity: 1, y: 0 };
 
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch('/landingan.json')
+      .then(response => response.json())
+      .then(data => setAnimationData(data));
+  }, []);
+
+
   return (
+    <>
     <Container
       fluid
       style={{
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        paddingTop: "10vh",
       }}
     >
-      <motion.div initial={initial} animate={animate} transition={{ delay: 0.1, duration: 0.5 }}>
-        <h1>hi, i'm aditya sahasranam.</h1>
-      </motion.div>
+      {animationData && <Lottie  style={{height:'350px', width:"350px"}}  animationData={animationData} />}
+      <h1>hi, i'm aditya sahasranam.</h1>
       <motion.div initial={initial} animate={animate} transition={{ delay: 0.2, duration: 0.5 }}>
         <h3>learning. coding. building. deploying. flying.</h3>
       </motion.div>
@@ -36,6 +50,7 @@ const LandingSection = () => {
         </Button>
       </motion.div>
     </Container>
+    </>
   );
 };
 
